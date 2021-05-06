69a40a9e4fc105fdb8668cfd9fe751de96d9c8b3
abdullahwale <abdullahwale@gmail.com>
2021-05-06 11:22:06 +0500

    12-modal-sheet 1. Further Input & Output Styling and Configuration 2.  Adding AppBar Buttons & Floating Action Buttons 3. Showing a Modal Bottom Sheet

--- new_expence_calculator/lib/main.dart
+++ new_expence_calculator/lib/main.dart
@@ -60,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           onTap: () {},
           child: NewTransaction(_addNewTransaction),
           behavior: HitTestBehavior.opaque,
+
         );
       },
     );
