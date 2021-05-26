9a7e4e208018b0032b0555a2dbf3e16074ba3b22
abdullahwale <abdullahwale@gmail.com>
2021-05-26 12:27:07 +0500

    setup the tiktok project

---
+++ expence_calculator_local/android/app/src/main/java/com/example/expence_calculator_local/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.expence_calculator_local;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ tiktokdemo/android/app/src/main/kotlin/com/example/tiktokdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tiktokdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
