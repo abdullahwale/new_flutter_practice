9311de11d561baaf35586ffdb12aadcf038df3fb
abdullahwale <abdullahwale@gmail.com>
2021-06-10 09:55:48 +0500

    09 - Add Some Code in video.dart

--- tiktokdemo/lib/pages/video.dart
+++ tiktokdemo/lib/pages/video.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktokdemo/variable.dart';
 
 class Video extends StatefulWidget {
   const Video({Key key}) : super(key: key);
@@ -10,6 +12,66 @@ class Video extends StatefulWidget {
 class _VideoState extends State<Video> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(color: Colors.black),
+          ),
+          Column(
+            //top Section
+            children: [
+              Container(
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Following",
+                      style: mystyle(17, Colors.white, FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      "For You",
+                      style: mystyle(17, Colors.white, FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              //Middle Section
+              Expanded(
+                  child: Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                      child: Container(
+                    height: 70,
+                    padding: EdgeInsets.only(left: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Abdullah",
+                            style: mystyle(15, Colors.white, FontWeight.bold)),
+                        Text("Cool Video ",
+                            style: mystyle(15, Colors.white, FontWeight.bold)),
+                      ],
+                    ),
+                  )),
+                ],
+              )),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
