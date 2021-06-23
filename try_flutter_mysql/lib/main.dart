3d026ebf69d2896c66851010f84ca442dfdd4558
abdullahwale <abdullahwale@gmail.com>
2021-06-23 15:41:29 +0500

    try data updated with API

--- try_flutter_mysql/lib/main.dart
+++ try_flutter_mysql/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -10,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,20 +23,23 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _incrementCounter() async {
+    //Uri uri = 'https://api.github.com/users/desi-programmer';
+    var url = Uri.parse('https://api.github.com/users/desi-programmer');
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    print(response);
+    // setState(() {
+    //   //_counter++;
+    // });
   }
 
   @override
@@ -42,25 +47,26 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        centerTitle: true,
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Your API Request Is: ',
             ),
-            Text(
+            /*Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
+            ),*/
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Make A Request',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
